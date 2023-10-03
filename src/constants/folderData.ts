6433a930306306072a1cf794ccee508bf9415405
jssatorres <juansataz.dev@gmail.com>
2023-10-03 18:09:00 +0200

    feat: first folder structure

---
+++ src/constants/folderData.ts
@@ -0,0 +1,29 @@
+type Folder = {
+  name: string
+  subfolders: Folder[]
+}
+
+export const folderData: Folder[] = [
+  {
+    name: 'Carpeta 1',
+    subfolders: [
+      {
+        name: 'Subcarpeta 1.1',
+        subfolders: [],
+      },
+    ],
+  },
+  {
+    name: 'Carpeta 2',
+    subfolders: [],
+  },
+  {
+    name: 'Carpeta 3',
+    subfolders: [
+      {
+        name: 'Subcarpeta 3.1',
+        subfolders: [],
+      },
+    ],
+  },
+]
