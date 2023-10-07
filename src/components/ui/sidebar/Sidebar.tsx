531d074b1f9fc0b0fafc4ac2f28214b1b6f22dca
jssatorres <juansataz.dev@gmail.com>
2023-10-07 13:30:26 +0200

    fix: dive sidebar component

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { folderData } from '../../../constants/folderData'
-
-type FolderProps = {
-  name: string
-  subfolders: FolderProps[]
-}
-
-const Folder: React.FC<FolderProps> = ({ name, subfolders }) => {
-  const [isExpanded, setIsExpanded] = useState(false)
-
-  const toggleExpansion = () => {
-    setIsExpanded(!isExpanded)
-  }
-
-  return (
-    <div>
-      <div
-        className={`px-2 py-1 cursor-pointer ${
-          isExpanded ? 'text-indigo-600' : 'text-gray-400'
-        }`}
-        onClick={toggleExpansion}
-      >
-        {isExpanded ? '▼' : '►'} {name}
-      </div>
-      {isExpanded && (
-        <div className="ml-4">
-          {subfolders.map((folder, index) => (
-            <Folder key={index} {...folder} />
-          ))}
-        </div>
-      )}
-    </div>
-  )
-}
+import { FolderComponent } from '../folderComponent/FolderComponent'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {folderData.map((folder, index) => (
-        <Folder key={index} {...folder} />
+        <FolderComponent
+          key={index}
+          subfolders={folder.subfolders}
+          name={folder.name}
+        />
       ))}
     </div>
   )
