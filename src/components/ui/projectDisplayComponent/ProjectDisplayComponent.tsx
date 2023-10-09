077521bd3009c2d2cfdf04bb28b6c5228e5e63c7
jssatorres <juansataz.dev@gmail.com>
2023-10-09 18:41:46 +0200

    feat: dd context

--- src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
+++ src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { ScreenDisplayContext } from '../../../context/ScreenDisplayContext'
 
 const ProjectDisplayComponent = () => {
   const [screenNumber, setScreenNumber] = useState(0)
+  const { stateScreenDisplay } = useContext(ScreenDisplayContext)
   const handleScreenNumber = () => {
     setScreenNumber((prev) => prev + 1)
   }
+  console.log(stateScreenDisplay)
+
   return (
     <>
       <div>Project Display Component</div>
-      <button onClick={handleScreenNumber}>+</button>
+      <button onClick={handleScreenNumber}>--+sdfsdf</button>
+      <div>sdsdf</div>
       <div>{screenNumber}</div>
     </>
   )
