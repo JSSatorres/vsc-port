78f6ee82067d2a3266d2ff6239ea3c798e8712c1
jssatorres <juansataz.dev@gmail.com>
2023-10-12 09:53:35 +0200

    fix: inital state

--- src/context/ScreenDisplayContext.tsx
+++ src/context/ScreenDisplayContext.tsx
@@ -1,5 +1,7 @@
 import { createContext } from 'react'
 import { type ScreenDisplayProviderContext } from '../types'
+import { INITIAL_STATE } from './ScreenDisplayProvider'
 
-export const ScreenDisplayContext =
-  createContext<ScreenDisplayProviderContext | null>(null)
+export const ScreenDisplayContext = createContext<ScreenDisplayProviderContext>(
+  { state: INITIAL_STATE, addProject: () => {}, deleteProject: () => {} },
+)
