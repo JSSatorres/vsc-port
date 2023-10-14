24619dcdb12dc6ec7c137f349ccfb671a1bb0e32
jssatorres <juansataz.dev@gmail.com>
2023-10-14 15:23:57 +0200

    feat: draw state compoments

--- src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
+++ src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
@@ -1,24 +1,21 @@
-import { useContext } from 'react'
-import { ScreenDisplayContext } from '../../../context/ScreenDisplayContext'
+import { useDisplayScreen } from '../../../hook/useDisplayScreen'
 import Project from '../Project/Project'
 
 const ProjectDisplayComponent = () => {
   // const [screenNumber, setScreenNumber] = useState(0)
-  const { state } = useContext(ScreenDisplayContext)
-  // const handleScreenNumber = () => {
-  //   setScreenNumber((prev) => prev + 1)
-  // }
+  useDisplayScreen
+  const { state } = useDisplayScreen()
+
   console.log(state)
-  // console.log(screenNumber)
 
   return (
     <section className="flex justify-start">
-      {Array.from({ length: state.screenNumber }).map((_, index) => (
-        <div key={index}>
-          <Project dataProject={state.currentScreenData[0]} />
-        </div>
-      ))}
-      {/* <button onClick={handleScreenNumber}>Add Screen</button> */}
+      {state.currentScreenData &&
+        state.currentScreenData.map((project) => (
+          <div key={project?.id}>
+            {project && <Project dataProject={project} />}
+          </div>
+        ))}
     </section>
   )
 }
