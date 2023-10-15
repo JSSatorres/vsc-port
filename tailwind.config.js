a9d018d04cd973df52eb95f15e96039985ae3ba2
jssatorres <juansataz.dev@gmail.com>
2023-10-15 10:38:42 +0200

    feat: add custom background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'dark-background': 'rgba(30, 34, 39, 0.979)',
+        darker: '#21242A',
+        darkest: '#1B1E22',
+        darke: '#1E2126',
+        'dark-terminal': '#181A1E',
+        grey: '#2D2F32',
+      },
+    },
   },
   plugins: [],
 }
-
