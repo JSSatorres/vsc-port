58d74eef0c5d0c9ac038cdc62056322cf7614083
jssatorres <juansataz.dev@gmail.com>
2023-10-15 10:38:18 +0200

    fix: coreected props

--- src/components/ui/Project/Project.tsx
+++ src/components/ui/Project/Project.tsx
@@ -4,19 +4,26 @@ interface ProjectProps {
   dataProject: Project
 }
 const Project: React.FC<ProjectProps> = ({ dataProject }) => {
-  const { name, language, mainTechnology, id, technologies } = dataProject
+  const { name, language, mainTechnology, technologies } = dataProject
 
   return (
-    <div className="text-white">
-      <h3>{name}</h3>
-      <h3>{id}</h3>
-      <h3>{language}</h3>
-      <h3>{mainTechnology}</h3>
-      <h3>
-        {technologies.map((technology, index) => (
-          <p key={index}>{technology}</p>
-        ))}
+    <div className="text-white my-5 mx-3">
+      <h3 className="text-4xl font-bold mb-4 flex flex-col items-center justify-center">
+        {name}
       </h3>
+
+      <p className="text-2xl text-gray-400">Language: {language}</p>
+      <p className="text-2xl text-gray-400">
+        Main Technology: {mainTechnology}
+      </p>
+      <div className="my-4">
+        <h2 className="text-3xl text-gray-400">Technologies:</h2>
+        <ul className="list-inside list-disc text-2xl">
+          {technologies.map((technology, index) => (
+            <li key={index}>{technology}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
