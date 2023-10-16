6027e82cfe64694634eecb98c0faabc301ec88fb
jssatorres <juansataz.dev@gmail.com>
2023-10-16 19:24:11 +0200

    feat: add dummy component

---
+++ src/components/ui/topBar/TopBar.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const TopBar = () => {
+  const [first, setfirst] = useState(second)
+  return (
+    <div>
+      <p>{first}</p>
+    </div>
+  )
+}
+
+export default TopBar
