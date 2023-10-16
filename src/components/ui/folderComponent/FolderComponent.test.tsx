ad34254b131b6d38bcfc9b66ce27e45afe3cd150
jssatorres <juansataz.dev@gmail.com>
2023-10-16 19:29:15 +0200

    feat: inital test structure

---
+++ src/components/ui/folderComponent/FolderComponent.test.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+// import { FolderComponent } from './FolderComponent'
+
+describe('<FolderComponent />', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
+    // ???
+  })
+})
