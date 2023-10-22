248afc550303622998ce748c74d1d23f40779c01
jssatorres <juansataz.dev@gmail.com>
2023-10-22 09:44:35 +0200

    feat: add styles

--- src/components/ui/SearchComponent/SearchButton/SearchButton.tsx
+++ src/components/ui/SearchComponent/SearchButton/SearchButton.tsx
@@ -7,12 +7,12 @@ interface SearchButtonProps {
 const SearchButton: React.FC<SearchButtonProps> = ({ handleShowSearch }) => {
   return (
     <button
-      className="flex justify-center bg-grey border rounded-md w-60 "
+      className="flex justify-center bg-grey border rounded-md w-60 search-button-container"
       onClick={handleShowSearch}
     >
       <IconSearch
         size={19}
-        className="text-gray-400 transform rotate-90 mt-1"
+        className="text-gray-400 transform rotate-90 mt-1 cursor-pointer"
       />
       <p className="text-gray-400 ml-2">Search a project</p>
     </button>
