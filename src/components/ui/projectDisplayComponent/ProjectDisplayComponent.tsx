c58912fe4f4dfd61a67c83f0ae7b40a89c5000d6
jssatorres <juansataz.dev@gmail.com>
2023-10-27 19:25:06 +0200

    fix: change hook

--- src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
+++ src/components/ui/projectDisplayComponent/ProjectDisplayComponent.tsx
@@ -1,17 +1,17 @@
-import { useDisplayScreen } from '../../../hook/useDisplayScreen'
+import { useDisplayScreen } from '../../../hooks/useDisplayScreen'
 import Project from '../Project/Project'
 
 const ProjectDisplayComponent = () => {
   // const [screenNumber, setScreenNumber] = useState(0)
   // useDisplayScreen
-  const { state } = useDisplayScreen()
+  const { currentScreenData } = useDisplayScreen()
 
-  console.log(state)
+  console.log(currentScreenData)
 
   return (
-    <section className="flex justify-start">
-      {state.currentScreenData &&
-        state.currentScreenData.map((project) => (
+    <section className="flex justify-start w-full">
+      {currentScreenData &&
+        currentScreenData.map((project) => (
           <div key={project?.id}>
             {project && <Project dataProject={project} />}
           </div>
