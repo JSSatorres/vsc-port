f9169e38579ca6932b97d6e7c4756a26bfe303f8
jssatorres <juansataz.dev@gmail.com>
2023-10-28 15:04:36 +0200

    fix: update context import

--- src/hooks/useDisplayScreenState.ts
+++ src/hooks/useDisplayScreenState.ts
@@ -1,12 +1,10 @@
 import { useContext } from 'react'
-import { screenDisplayContextState } from '../context/ScreenDisplayContext'
+import { screenDisplayStateContext } from '../context/ScreenDisplayContext'
 
 export const useDisplayScreenState = () => {
-  const context = useContext(screenDisplayContextState)
-  if (context === undefined) {
-    throw new Error(
-      'useDisplayScreenState must be used within a useDisplayScreenState',
-    )
+  const context = useContext(screenDisplayStateContext)
+  if (!context) {
+    throw new Error('useDisplayScreenState must be used within a Provider')
   }
   return context
 }
