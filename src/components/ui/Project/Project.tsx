b27e45d532bc6fd59a5ebc001c239531a6242037
JSSatorres <juansataz.dev@gmail.com>
2024-01-12 21:06:15 +0100

    feat: add styles

--- src/components/ui/Project/Project.tsx
+++ src/components/ui/Project/Project.tsx
@@ -1,13 +1,24 @@
-import { type Project } from '../../../types'
+import { useDisplayScreen } from '../../../hooks/useDisplayScreen'
+import { IdFormat, type Project } from '../../../types'
+import { IconX } from '@tabler/icons-react'
 
 interface ProjectProps {
   dataProject: Project
 }
 const Project: React.FC<ProjectProps> = ({ dataProject }) => {
-  const { name, language, mainTechnology, technologies } = dataProject
+  const { name, language, mainTechnology, technologies, id } = dataProject
+  const { deleteProjectDispatch } = useDisplayScreen()
+
+  const handleDelete = (id: IdFormat): void => {
+    deleteProjectDispatch(id)
+  }
 
   return (
-    <div className="text-white my-5 mx-3">
+    <div className="flex flex-col text-white py-5 px-3 border-2 min-h-max border-shadow-2xl animate-bounce-slow items-center  justify-center">
+      <IconX
+        className="cursor-pointer ml-4 mr-1 hover:bg-grey place-self-end"
+        onClick={() => handleDelete(id)}
+      />
       <h3 className="text-4xl font-bold mb-4 flex flex-col items-center justify-center">
         {name}
       </h3>
